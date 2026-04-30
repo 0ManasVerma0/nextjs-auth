1a3c2d7f68ee502a06fa4feb7714fb6d9e2e2905
0ManasVerma0 <manasverma1357@gmail.com>
2026-04-30 23:16:57 +0530

    add fix

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,12 +1,28 @@
-export default function UserProfile({params}: any) {
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel.js";
+
+type ProfilePageProps = {
+    params: Promise<{
+        id: string;
+    }>;
+};
+
+export default async function UserProfile({ params }: ProfilePageProps) {
+    const { id } = await params;
+
+    await connect();
+    const user = await User.findById(id).select("-password");
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
-            <p className="text-4xl">Profile page 
-            <span className=" p-2 ml-2 rounded bg-orange-500 text-black">{params.id}</span>
+            <p className="text-4xl">
+                Profile page
+                <span className=" p-2 ml-2 rounded bg-orange-500 text-black">
+                    {user?.username ?? "User not found"}
+                </span>
             </p>
-
-            </div>
-    )
+        </div>
+    );
 }
